c70dc833836922f1980c4bfc49e7494309eab77d
Calvin McDowell <calvinmac40@hotmail.com>
2019-10-02 23:59:40 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  attr_reader :name, :owner, :species
+  
+  @@all = []
+  @@count = 0
+  @@reset_all = []
+  
+  def initialize(name)
+    @name = name
+    @owner = owner
+    @species = "human"
+    @@all << self
+  end
+  
+  def say_species
+    @say_species = "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length {|num| num }
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+  
+  def dogs 
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
 end
